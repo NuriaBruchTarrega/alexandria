4b6e07d7b667c71609d5a55384bfe20edb74f66c
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-05-12 12:22:58 +0200

    Usage of FileManager from Analyzer

--- backend/src/main/java/nl/uva/alexandria/logic/Analyzer.java
+++ backend/src/main/java/nl/uva/alexandria/logic/Analyzer.java
@@ -1,5 +1,6 @@
 package nl.uva.alexandria.logic;
 
+import nl.uva.alexandria.utils.FileManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -15,7 +16,9 @@ public class Analyzer {
         this.parser = parser;
     }
 
-    public void analyze(String path, String clientLibrary) {
-        LOG.info("Analyze");
+    public void analyze(String pathToClientLibraryJarFolder, String clientLibrary) {
+        // Obtain Jar
+        FileManager fileManager = new FileManager();
+        String clientLibraryJar = fileManager.getClientLibraryPath(pathToClientLibraryJarFolder, clientLibrary);
     }
 }
--- backend/src/main/java/nl/uva/alexandria/utils/FileManager.java
+++ backend/src/main/java/nl/uva/alexandria/utils/FileManager.java
@@ -1,7 +1,6 @@
 package nl.uva.alexandria.utils;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,11 +9,15 @@ public class FileManager {
     public FileManager() {
     }
 
-    public static String getClientLibraryPath(String pathToClientLibraryFolder, String clientLibraryJar) throws FileNotFoundException {
+    public String getClientLibraryPath(String pathToClientLibraryFolder, String clientLibraryJar) {
         File clientLibraryFolder = new File(pathToClientLibraryFolder);
         File[] filesInFolder = clientLibraryFolder.listFiles();
+        assert filesInFolder != null : "Folder is empty";
+
         List<File> filteredFiles = Arrays.stream(filesInFolder).filter(file -> file.getName().contains(clientLibraryJar) && file.getName().contains(".jar")).collect(Collectors.toList());
-        if (filteredFiles.size() != 1) throw new FileNotFoundException();
+        assert filteredFiles.size() != 0 : "No client Library jar found";
+        assert filteredFiles.size() == 1 : "More than one jar match client library name";
+
         return filteredFiles.get(0).getAbsolutePath();
     }
 }
