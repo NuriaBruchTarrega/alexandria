f3429638f335d4886645908d84b5c5ef55639a27
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-05-12 12:57:48 +0200

    Add getServerLibrariesJarPaths in FileManager

--- backend/src/main/java/nl/uva/alexandria/logic/Analyzer.java
+++ backend/src/main/java/nl/uva/alexandria/logic/Analyzer.java
@@ -1,9 +1,13 @@
 package nl.uva.alexandria.logic;
 
+import javassist.ClassPool;
+import javassist.NotFoundException;
 import nl.uva.alexandria.utils.FileManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class Analyzer {
@@ -17,8 +21,14 @@ public class Analyzer {
     }
 
     public void analyze(String pathToClientLibraryJarFolder, String clientLibrary) {
-        // Obtain Jar
+        // Obtain client library Jar
         FileManager fileManager = new FileManager();
-        String clientLibraryJar = fileManager.getClientLibraryPath(pathToClientLibraryJarFolder, clientLibrary);
+        String clientLibraryJar = fileManager.getClientLibraryJarPath(pathToClientLibraryJarFolder, clientLibrary);
+
+        // Obtain all dependency jar files using maven invoker
+        // TODO: discover how to use the maven invoker
+
+        // Obtain all server libraries jar file names.
+        List<String> serverLibrariesJars = fileManager.getServerLibrariesJarPaths(pathToClientLibraryJarFolder);
     }
 }
--- backend/src/main/java/nl/uva/alexandria/utils/FileManager.java
+++ backend/src/main/java/nl/uva/alexandria/utils/FileManager.java
@@ -9,15 +9,28 @@ public class FileManager {
     public FileManager() {
     }
 
-    public String getClientLibraryPath(String pathToClientLibraryFolder, String clientLibraryJar) {
+    public String getClientLibraryJarPath(String pathToClientLibraryFolder, String clientLibraryJar) {
         File clientLibraryFolder = new File(pathToClientLibraryFolder);
         File[] filesInFolder = clientLibraryFolder.listFiles();
         assert filesInFolder != null : "Folder is empty";
 
-        List<File> filteredFiles = Arrays.stream(filesInFolder).filter(file -> file.getName().contains(clientLibraryJar) && file.getName().contains(".jar")).collect(Collectors.toList());
+        List<File> filteredFiles = Arrays.stream(filesInFolder).filter(file -> file.getName().contains(clientLibraryJar) && file.getName().endsWith(".jar")).collect(Collectors.toList());
         assert filteredFiles.size() != 0 : "No client Library jar found";
         assert filteredFiles.size() == 1 : "More than one jar match client library name";
 
         return filteredFiles.get(0).getAbsolutePath();
+    }
+
+    public List<String> getServerLibrariesJarPaths(String pathToClientLibraryFolder) {
+        File dependencyFolder = new File(pathToClientLibraryFolder + "/target/dependency");
+        File[] filesInFolder = dependencyFolder.listFiles();
+        assert filesInFolder != null : "Folder is empty";
+
+        List<File> filteredFiles = Arrays.stream(filesInFolder).filter(file -> file.getName().endsWith(".jar")).collect(Collectors.toList());
+        assert filteredFiles.size() != 0 : "No jars found";
+
+        List<String> jarPaths = filteredFiles.stream().map(file -> file.getAbsolutePath()).collect(Collectors.toList());
+
+        return jarPaths;
     }
 }
