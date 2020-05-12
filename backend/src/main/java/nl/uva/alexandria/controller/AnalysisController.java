e18d79db6711281ee063628e36b7e9fe03112feb
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-05-12 10:32:48 +0200

    Modified request

--- backend/src/main/java/nl/uva/alexandria/controller/AnalysisController.java
+++ backend/src/main/java/nl/uva/alexandria/controller/AnalysisController.java
@@ -1,22 +1,22 @@
 package nl.uva.alexandria.controller;
 
 import nl.uva.alexandria.logic.Analyzer;
-import nl.uva.alexandria.model.dto.request.GreetingRequest;
+import nl.uva.alexandria.model.dto.request.AnalysisRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class AnalysisController {
 
     private final Analyzer analyzer;
 
-    public HelloController(Analyzer analyzer) {
+    public AnalysisController(Analyzer analyzer) {
         this.analyzer = analyzer;
     }
 
-    @PostMapping("/greeting")
-    public String greeting(@RequestBody GreetingRequest request) {
-        return analyzer.greeting(request.getName());
+    @PostMapping("/analyze")
+    public void analyze(@RequestBody AnalysisRequest request) {
+        analyzer.analyze(request.getClientJarPath(), request.getClientIdentifier());
     }
 }
---
+++ backend/src/main/java/nl/uva/alexandria/model/dto/request/AnalysisRequest.java
@@ -0,0 +1,15 @@
+package nl.uva.alexandria.model.dto.request;
+
+public class AnalysisRequest {
+
+    private String clientJarPath;
+    private String clientIdentifier;
+
+    public String getClientJarPath() {
+        return clientJarPath;
+    }
+
+    public String getClientIdentifier() {
+        return clientIdentifier;
+    }
+}
