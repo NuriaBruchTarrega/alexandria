cc9590b256b61d592da839cc671e0b086cfc9de5
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-06-29 17:35:40 +0200

    Implementation of getCallGraphs

---
+++ backend/src/main/java/nl/uva/alexandria/model/sattose/Factory.java
@@ -0,0 +1,10 @@
+package nl.uva.alexandria.model.sattose;
+
+import org.json.simple.JSONObject;
+
+public class Factory {
+
+    public static LibraryCallGraph getLibraryCallGraphFromJson(JSONObject libraryJson) {
+        return null;
+    }
+}
--- backend/src/main/java/nl/uva/alexandria/sattose/CallGraphAnalyzer.java
+++ backend/src/main/java/nl/uva/alexandria/sattose/CallGraphAnalyzer.java
@@ -1,14 +1,31 @@
 package nl.uva.alexandria.sattose;
 
+import nl.uva.alexandria.model.sattose.Factory;
+import nl.uva.alexandria.model.sattose.LibraryCallGraph;
 import nl.uva.alexandria.model.sattose.SattoseFiles;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 public class CallGraphAnalyzer {
     public void analyze(String pathToFolder) {
         SattoseFiles sattoseFiles = getSattoseFiles(pathToFolder);
+        try {
+            Set<LibraryCallGraph> callGraphs = getLibraryCallGraphsFromFile(sattoseFiles);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         obtainMostUsedMethods(sattoseFiles);
     }
 
@@ -21,6 +38,21 @@ public class CallGraphAnalyzer {
             else if (file.getName().contains("sattose_dependencies_final")) sattoseFiles.libraryDataTxt = file;
         }
         return sattoseFiles;
+    }
+
+    private Set<LibraryCallGraph> getLibraryCallGraphsFromFile(SattoseFiles sattoseFiles) throws IOException, ParseException {
+        Set<LibraryCallGraph> callGraphs = new HashSet<>();
+        JSONParser parser = new JSONParser();
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(sattoseFiles.callGraphsJson));
+        String line = bufferedReader.readLine();
+
+        while (line != null) {
+            JSONObject callGraphJSON = (JSONObject) parser.parse(line);
+            callGraphs.add(Factory.getLibraryCallGraphFromJson(callGraphJSON));
+            line = bufferedReader.readLine();
+        }
+
+        return callGraphs;
     }
 
     private void obtainMostUsedMethods(SattoseFiles sattoseFiles) {
