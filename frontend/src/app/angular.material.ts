7c06dacc4b4531cfb5902d5f09fd03380a553e8c
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-07-09 15:26:00 +0200

    Add toolbar and router outlet in base component

--- frontend/src/app/angular.material.ts
+++ frontend/src/app/angular.material.ts
@@ -1,8 +1,14 @@
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 export const MAT_MODULES = [
   BrowserAnimationsModule,
   FormsModule,
-  ReactiveFormsModule
+  ReactiveFormsModule,
+  MatTooltipModule,
+  MatToolbarModule,
+  MatIconModule
 ];
