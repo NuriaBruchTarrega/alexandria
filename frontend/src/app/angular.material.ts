3e7c4a2cd85b610299eb6550274c2c350dca2314
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-07-09 17:54:36 +0200

    Add mat card with first form field

--- frontend/src/app/angular.material.ts
+++ frontend/src/app/angular.material.ts
@@ -3,6 +3,9 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
 
 export const MAT_MODULES = [
   BrowserAnimationsModule,
@@ -10,5 +13,8 @@ export const MAT_MODULES = [
   ReactiveFormsModule,
   MatTooltipModule,
   MatToolbarModule,
-  MatIconModule
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatCardModule
 ];
