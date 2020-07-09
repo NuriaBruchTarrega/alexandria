216f1647ff14c615d779bff8356e4f847bbb0937
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-07-09 10:41:15 +0200

    Add calculatePolymorphismOfDependency with DependencyTreeNode in PolymorphismDetection

--- backend/src/main/java/nl/uva/alexandria/logic/metrics/PolymorphismDetection.java
+++ backend/src/main/java/nl/uva/alexandria/logic/metrics/PolymorphismDetection.java
@@ -2,15 +2,14 @@ package nl.uva.alexandria.logic.metrics;
 
 import javassist.*;
 import nl.uva.alexandria.logic.ClassPoolManager;
+import nl.uva.alexandria.model.DependencyTreeNode;
 import nl.uva.alexandria.model.Library;
+import nl.uva.alexandria.model.ReachableMethods;
 import nl.uva.alexandria.model.ServerMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static nl.uva.alexandria.logic.utils.GeneralUtils.stackTraceToString;
@@ -69,5 +68,43 @@ class PolymorphismDetection {
         }
 
         return mapPolymorphicImplementations;
+    }
+
+    public static void calculatePolymorphismOfDependency(DependencyTreeNode dependencyTreeNode, ClassPoolManager classPoolManager) throws NotFoundException {
+        Set<CtClass> libraryClasses = classPoolManager.getLibraryClasses(dependencyTreeNode.getLibrary().getLibraryPath());
+        Map<Integer, ReachableMethods> reachableMethodsAtDistance = dependencyTreeNode.getReachableMethodsAtDistance();
+
+        for (CtClass libraryClass : libraryClasses) {
+            reachableMethodsAtDistance.forEach((distance, reachability) -> {
+                Map<CtBehavior, Integer> polymorphicImplementations = new HashMap<>();
+                reachability.getReachableMethods().forEach((reachableMethod, numLines) -> {
+                    Optional<CtBehavior> polymorphicImplementationOpt = findPolymorphicImplementation(libraryClass, reachableMethod);
+                    polymorphicImplementationOpt.ifPresent(behavior -> polymorphicImplementations.put(behavior, numLines));
+                });
+                reachability.addMultipleReachableMethods(polymorphicImplementations);
+            });
+        }
+    }
+
+    private static Optional<CtBehavior> findPolymorphicImplementation(CtClass libraryClass, CtBehavior reachableMethod) {
+        if (libraryClass.equals(reachableMethod.getDeclaringClass())) return Optional.empty();
+        if (!libraryClass.subclassOf(reachableMethod.getDeclaringClass())) return Optional.empty();
+
+        try {
+            CtBehavior foundBehavior = null;
+            if (reachableMethod instanceof CtMethod) {
+                // If there is no exception, the method has been found
+                foundBehavior = libraryClass.getDeclaredMethod(reachableMethod.getName(), reachableMethod.getParameterTypes());
+            } else if (reachableMethod instanceof CtConstructor) {
+                // If there is no exception, the constructor has been found
+                foundBehavior = libraryClass.getConstructor(reachableMethod.getSignature());
+            } else return Optional.empty(); // In case at some point there is another type of behavior
+
+            // Do something with the polymorphic implementation found
+            return Optional.of(foundBehavior);
+        } catch (NotFoundException e) {
+            // Class does not have polymorphic implementation of the method
+            return Optional.empty();
+        }
     }
 }
