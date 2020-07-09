4e1fbcb9ffb5ccc39bd809a7b1453e4de6607294
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-07-09 15:35:25 +0200

    Add title of the form

--- frontend/src/app/analyzer/analyzer.component.ts
+++ frontend/src/app/analyzer/analyzer.component.ts
@@ -6,6 +6,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./analyzer.component.css']
 })
 export class AnalyzerComponent implements OnInit {
+  title = 'Specify client library';
 
   constructor() {
   }
