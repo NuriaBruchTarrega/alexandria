107f72463a9c653707cb23484ebd251618e05b13
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-07-10 10:28:51 +0200

    Add analyze request in analyzer service

--- frontend/src/app/services/analyzer.service.ts
+++ frontend/src/app/services/analyzer.service.ts
@@ -1,10 +1,19 @@
 import {Injectable} from '@angular/core';
+import {environment} from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+
+const apiBaseUrl = environment.apiBaseUrl;
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnalyzerService {
 
-  constructor() {
+  constructor(private httpClient: HttpClient) {
   }
+
+  analyzeLibrary(groupID: string, artifactID: string, version: string) {
+    const result = this.httpClient.post(`${apiBaseUrl}/analyze`, {groupID, artifactID, version});
+  }
+
 }
