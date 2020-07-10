2dafea344491e285ace5f30fccc5ece072de0b00
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-07-10 12:25:37 +0200

    Add usage of builder in analyze service

--- frontend/src/app/services/analyzer.service.ts
+++ frontend/src/app/services/analyzer.service.ts
@@ -1,19 +1,19 @@
 import {Injectable} from '@angular/core';
 import {environment} from '../../environments/environment';
 import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import {buildDependencyGraph} from '../builders/analyzer.builder';
 
 const apiBaseUrl = environment.apiBaseUrl;
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class AnalyzerService {
 
   constructor(private httpClient: HttpClient) {
   }
 
   analyzeLibrary(groupID: string, artifactID: string, version: string) {
-    const result = this.httpClient.post(`${apiBaseUrl}/analyze`, {groupID, artifactID, version});
+    const result = this.httpClient.post(`${apiBaseUrl}/analyze`, {groupID, artifactID, version})
+      .pipe(map(res => buildDependencyGraph(res)));
   }
-
 }
