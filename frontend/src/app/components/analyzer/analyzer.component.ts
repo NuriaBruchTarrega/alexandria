224d4e635aa311ba34d373033109716d8e1e3a2d
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-07-13 12:23:20 +0200

    Add display tree after request

--- frontend/src/app/components/analyzer/analyzer.component.ts
+++ frontend/src/app/components/analyzer/analyzer.component.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {AnalyzerService} from '../../services/analyzer.service';
 import {Library} from '../../models/library';
+import {VisualizationComponent} from './visualization/visualization.component';
+import {DependencyTreeFactory} from '../../models/dependencyTree/tree';
 
 @Component({
   selector: 'analyzer',
@@ -8,6 +10,7 @@ import {Library} from '../../models/library';
   styleUrls: ['./analyzer.component.css']
 })
 export class AnalyzerComponent implements OnInit {
+  @ViewChild('treeVisualization') treeVisualization: VisualizationComponent;
 
   constructor(private analyzerService: AnalyzerService) {
   }
@@ -20,7 +23,27 @@ export class AnalyzerComponent implements OnInit {
       .analyzeLibrary(library)
       .subscribe(res => {
         console.log(res);
+        this.updateTreeVisualization();
       });
+  }
+
+  private updateTreeVisualization() {
+    const nodes = [
+      {id: 1, label: 'Node 1', title: 'I am node 1!'},
+      {id: 2, label: 'Node 2', title: 'I am node 2!'},
+      {id: 3, label: 'Node 3'},
+      {id: 4, label: 'Node 4'},
+      {id: 5, label: 'Node 5'}
+    ];
+
+    const edges = [
+      {from: 1, to: 3},
+      {from: 1, to: 2},
+      {from: 2, to: 4},
+      {from: 2, to: 5}
+    ];
+
+    this.treeVisualization.generateVisTree(DependencyTreeFactory.create({nodes, edges}));
   }
 
 }
--- frontend/src/app/components/analyzer/visualization/visualization.component.ts
+++ frontend/src/app/components/analyzer/visualization/visualization.component.ts
@@ -1,7 +1,7 @@
 import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import {Network} from 'vis-network';
 import {options} from './options';
-import {DependencyTree, DependencyTreeFactory} from '../../../models/dependencyTree/tree';
+import {DependencyTree} from '../../../models/dependencyTree/tree';
 
 @Component({
   selector: 'visualization',
@@ -29,28 +29,6 @@ export class VisualizationComponent implements AfterViewInit {
     });
     this.network.on('blurNode', params => {
       console.log('blurNode event:', params);
-    });
-  }
-
-  getTreeData(): DependencyTree {
-    const nodes = [
-      {id: 1, label: 'Node 1', title: 'I am node 1!'},
-      {id: 2, label: 'Node 2', title: 'I am node 2!'},
-      {id: 3, label: 'Node 3'},
-      {id: 4, label: 'Node 4'},
-      {id: 5, label: 'Node 5'}
-    ];
-
-    const edges = [
-      {from: 1, to: 3},
-      {from: 1, to: 2},
-      {from: 2, to: 4},
-      {from: 2, to: 5}
-    ];
-
-    return DependencyTreeFactory.create({
-      nodes,
-      edges
     });
   }
 }
