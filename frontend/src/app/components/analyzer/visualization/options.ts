ac71499026cd4c617ae086fdc433fee6b87d76ff
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-07-13 15:49:47 +0200

    Add hierarchical layout

--- frontend/src/app/components/analyzer/visualization/options.ts
+++ frontend/src/app/components/analyzer/visualization/options.ts
@@ -4,5 +4,19 @@ export const options = {
   },
   manipulation: {
     enabled: false
+  },
+  layout: {
+    hierarchical: {
+      sortMethod: 'directed',
+      direction: 'UD',
+      nodeSpacing: 300,
+      levelSeparation: 100
+    }
+  },
+  physics: {
+    enabled: false,
+    repulsion: {
+      nodeDistance: 300
+    }
   }
 };
