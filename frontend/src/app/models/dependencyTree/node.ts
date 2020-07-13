e38f5074a580073356b6784c98664f3aeef14701
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-07-13 16:38:42 +0200

    Add md font in nodes

--- frontend/src/app/models/dependencyTree/node.ts
+++ frontend/src/app/models/dependencyTree/node.ts
@@ -1,23 +1,24 @@
 export class TreeNodeFactory {
-  static create({id = 0, label = '', title = ''}): TreeNode {
-    return new TreeNode(id, label, title);
+  static create({id = 0, label = '', title = '', font = {multi: 'md'}}): TreeNode {
+    return new TreeNode(id, label, title, font);
   }
 }
 
 export interface ITreeNode {
   id: number;
   label: string;
-  title: string;
 }
 
 export class TreeNode implements ITreeNode {
   id: number;
-  label: string;
-  title: string;
+  label: string; // Content of the node
+  title: string; // Tooltip
+  font: any;
 
-  constructor(id: number, label: string, title: string) {
+  constructor(id: number, label: string, title: string, font: any) {
     this.id = id;
     this.label = label;
     this.title = title;
+    this.font = font;
   }
 }
