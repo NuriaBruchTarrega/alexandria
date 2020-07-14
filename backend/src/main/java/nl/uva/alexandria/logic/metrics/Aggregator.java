3faccda037b4e3c785f3bf2c0ec9dce6b7ac6ad1
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-07-14 14:49:47 +0200

    Delete unused method from Aggregator

--- backend/src/main/java/nl/uva/alexandria/logic/metrics/Aggregator.java
+++ backend/src/main/java/nl/uva/alexandria/logic/metrics/Aggregator.java
@@ -4,24 +4,12 @@ import javassist.CtBehavior;
 import javassist.CtClass;
 import nl.uva.alexandria.model.DependencyTreeNode;
 import nl.uva.alexandria.model.DependencyTreeResult;
-import nl.uva.alexandria.model.Library;
-import nl.uva.alexandria.model.ServerClass;
 
 import java.util.Map;
 
 public class Aggregator {
 
-    public static Map<Library, Integer> joinByLibrary(Map<? extends ServerClass, Integer> mapToJoin, Map<Library, Integer> joinedByLibrary) {
-        mapToJoin.forEach((serverClass, num) -> {
-            Library library = serverClass.getLibrary();
-            joinedByLibrary.computeIfPresent(library, (key, value) -> value + num);
-            joinedByLibrary.putIfAbsent(library, num);
-        });
-
-        return joinedByLibrary;
-    }
-
-    public static DependencyTreeResult calculateMethodInvocationCoupling(DependencyTreeNode dependencyTree) {
+    public static DependencyTreeResult calculateResultTree(DependencyTreeNode dependencyTree) {
         return createResultTree(dependencyTree);
     }
 
