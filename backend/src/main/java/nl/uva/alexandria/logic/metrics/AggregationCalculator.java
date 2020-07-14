30b09960dc8ee68e9a022faf25cf43a7128fc07b
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-07-14 14:43:42 +0200

    Update AggregationCalculator to calculate AC with DependencyTreeNode

--- backend/src/main/java/nl/uva/alexandria/logic/metrics/AggregationCalculator.java
+++ backend/src/main/java/nl/uva/alexandria/logic/metrics/AggregationCalculator.java
@@ -5,8 +5,10 @@ import javassist.CtField;
 import javassist.NotFoundException;
 import nl.uva.alexandria.logic.ClassPoolManager;
 import nl.uva.alexandria.logic.utils.ClassNameUtils;
+import nl.uva.alexandria.model.DependencyTreeNode;
+import nl.uva.alexandria.model.Library;
 import nl.uva.alexandria.model.ServerClass;
-import nl.uva.alexandria.model.factories.ServerClassFactory;
+import nl.uva.alexandria.model.factories.LibraryFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,65 +28,73 @@ public class AggregationCalculator {
     public Map<ServerClass, Integer> calculateAggregationCoupling() {
         // Loop through all the classes to find stable fields declared in them
         Set<CtClass> clientClasses = classPoolManager.getClientClasses();
-        computeStableDeclaredFields(clientClasses);
+        computeStableDeclaredFields(clientClasses, dependencyTreeNode);
 
         // Find descendants
-        Map<ServerClass, Integer> mapAcDescendants = DescendantsDetector.countDescendants(stableDeclaredFields, classPoolManager);
+        // Map<ServerClass, Integer> mapAcDescendants = DescendantsDetector.countDescendants(stableDeclaredFields, classPoolManager);
 
         return mapAcDescendants;
     }
 
-    private void computeStableDeclaredFields(Set<CtClass> clientClasses) {
+    //MEASURE DIRECT DEPENDENCIES
+    private void computeStableDeclaredFields(Set<CtClass> clientClasses, DependencyTreeNode dependencyTreeNode) {
         clientClasses.forEach(clientClass -> {
             // Get all fields
             CtField[] fields = clientClass.getDeclaredFields();
             // Get all stable types
             for (CtField field : fields) {
                 if (field.getGenericSignature() != null) {
-                    computeFieldWithGeneric(field); // It has generic type
+                    computeFieldWithGeneric(field, dependencyTreeNode); // It has generic type
                 } else {
-                    computeSimpleField(field); // It is a simple type
+                    computeSimpleField(field, dependencyTreeNode); // It is a simple type
                 }
             }
         });
     }
 
-    private void computeFieldWithGeneric(CtField field) {
+    // Methods to compute the different fields
+    private void computeFieldWithGeneric(CtField field, DependencyTreeNode dependencyTreeNode) {
         String gen = field.getGenericSignature();
         List<String> classNames = ClassNameUtils.getClassNamesFromGenericSignature(gen);
         Set<CtClass> classes = classPoolManager.getClassesFromClassNames(classNames);
-        classes.forEach(this::computeClass);
+        classes.forEach(clazz -> computeField(clazz, dependencyTreeNode));
     }
 
-    private void computeSimpleField(CtField field) {
+    private void computeSimpleField(CtField field, DependencyTreeNode dependencyTreeNode) {
         try {
             CtClass serverClass = field.getType();
             if (serverClass.isPrimitive()) return; // Ignore primitives
             if (serverClass.isArray()) {
                 Optional<CtClass> arrayType = getTypeOfArray(field);
                 if (arrayType.isEmpty() || arrayType.get().isPrimitive()) return;
-                computeClass(arrayType.get());
+                computeField(arrayType.get(), dependencyTreeNode);
             } else {
-                computeClass(serverClass);
+                computeField(serverClass, dependencyTreeNode);
             }
         } catch (NotFoundException e) {
             LOG.warn("Not able to find class of field: " + field.getSignature());
         }
     }
 
-    private void computeClass(CtClass clazz) {
+    private void computeField(CtClass clazz, DependencyTreeNode dependencyTreeNode) {
         try {
             // Filter out everything that is not in the server libraries
             if (classPoolManager.isClassInDependency(clazz)) {
-                String path = clazz.getURL().getPath();
-                ServerClass serverClass = ServerClassFactory.getServerClassFromCtClass(clazz, path);
-                stableDeclaredFields.computeIfPresent(serverClass, (key, value) -> value + 1);
-                stableDeclaredFields.putIfAbsent(serverClass, 1);
+                addReachableField(clazz, dependencyTreeNode, 1, 1);
             }
         } catch (NotFoundException e) {
             LOG.warn("Not found URL of class: " + clazz.getName());
         }
 
+    }
+
+    // SHARED IN DIRECT AND TRANSITIVE
+    private void addReachableField(CtClass ctClass, DependencyTreeNode dependencyTreeNode, Integer distance, Integer numAffectedLines) throws NotFoundException {
+        Library serverLibrary = LibraryFactory.getLibraryFromClassPath(ctClass.getURL().getPath());
+        Optional<DependencyTreeNode> libraryNode = dependencyTreeNode.findLibraryNode(serverLibrary);
+        if (libraryNode.isPresent()) {
+            libraryNode.get().addReachableApiField(distance, ctClass, numAffectedLines);
+        } else LOG.warn("Library not found in tree: {}", serverLibrary.toString());
     }
 
     private Optional<CtClass> getTypeOfArray(CtField field) throws NotFoundException {
