721beb3844d9e196430d553caa78701aacfd0263
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-07-20 11:46:27 +0200

    Update ReachableMethods with Set<MethodCall> instead of Integer

--- backend/src/main/java/nl/uva/alexandria/model/ReachableMethods.java
+++ backend/src/main/java/nl/uva/alexandria/model/ReachableMethods.java
@@ -1,24 +1,29 @@
 package nl.uva.alexandria.model;
 
 import javassist.CtBehavior;
+import javassist.expr.MethodCall;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class ReachableMethods {
 
-    private Map<CtBehavior, Integer> reachableMethods = new HashMap<>();
+    private Map<CtBehavior, Set<MethodCall>> reachableMethods = new HashMap<>();
 
-    public Map<CtBehavior, Integer> getReachableMethods() {
+    public Map<CtBehavior, Set<MethodCall>> getReachableMethods() {
         return reachableMethods;
     }
 
-    public void addReachableMethod(CtBehavior ctBehavior, Integer numCallsInClient) {
-        this.reachableMethods.computeIfPresent(ctBehavior, (key, value) -> value + numCallsInClient);
-        this.reachableMethods.putIfAbsent(ctBehavior, numCallsInClient);
+    public void addReachableMethod(CtBehavior ctBehavior, Set<MethodCall> reachableFrom) {
+        this.reachableMethods.computeIfPresent(ctBehavior, (key, value) -> {
+            value.addAll(reachableFrom);
+            return value;
+        });
+        this.reachableMethods.putIfAbsent(ctBehavior, reachableFrom);
     }
 
-    public void addMultipleReachableMethods(Map<CtBehavior, Integer> newReachableMethods) {
+    public void addMultipleReachableMethods(Map<CtBehavior, Set<MethodCall>> newReachableMethods) {
         newReachableMethods.forEach(this::addReachableMethod);
     }
 }
