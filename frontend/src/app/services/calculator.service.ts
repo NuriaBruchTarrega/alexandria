8a8b18bb110086608bae6d6a73c540d45e6b00f5
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-07-21 12:42:26 +0200

    Add CalculatorService calls calculateTMIC and calculateTAC on DependencyTree

--- frontend/src/app/services/calculator.service.ts
+++ frontend/src/app/services/calculator.service.ts
@@ -12,17 +12,9 @@ export class CalculatorService {
 
   calculateMetric(dependencyTree: DependencyTree, metric: Metrics, factor: number) {
     if (metric === Metrics.Tmic) {
-      this.calculateTMIC(dependencyTree, factor);
+      dependencyTree.calculateTMIC(factor);
     } else if (metric === Metrics.Tac) {
-      this.calculateTAC(dependencyTree, factor);
+      dependencyTree.calculateTAC(factor);
     }
-  }
-
-  private calculateTMIC(dependencyTree: DependencyTree, factor: number) {
-    // Calculate TMIC
-  }
-
-  private calculateTAC(dependencyTree: DependencyTree, factor: number) {
-    // Calculate TAC
   }
 }
