b1e0a61483a639d2c5b3269eed95656db18586a0
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-07-21 17:19:53 +0200

    Aggregator calculates classDistribution of the metrics

--- backend/src/main/java/nl/uva/alexandria/logic/metrics/Aggregator.java
+++ backend/src/main/java/nl/uva/alexandria/logic/metrics/Aggregator.java
@@ -19,20 +19,41 @@ public class Aggregator {
     private static DependencyTreeResult createResultTree(DependencyTreeNode dependencyTree) {
         DependencyTreeResult dependencyTreeResult = new DependencyTreeResult(dependencyTree.getLibrary());
 
-        dependencyTree.getReachableMethodsAtDistance().forEach((distance, reachability) -> {
-            Map<CtBehavior, Set<MethodCall>> reachableMethods = reachability.getReachableMethods();
-            Integer result = reachableMethods.values().stream().map(Set::size).reduce(0, Integer::sum);
-            dependencyTreeResult.addMicAtDistance(distance, result);
-        });
-
-        dependencyTree.getReachableFieldsAtDistance().forEach((distance, reachability) -> {
-            Map<CtClass, Set<CtField>> reachableFields = reachability.getReachableFields();
-            Integer result = reachableFields.values().stream().map(Set::size).reduce(0, Integer::sum);
-            dependencyTreeResult.addAcAtDistance(distance, result);
-        });
+        aggregateMIC(dependencyTree, dependencyTreeResult);
+        aggregateAC(dependencyTree, dependencyTreeResult);
 
         dependencyTree.getChildren().forEach(child -> dependencyTreeResult.addChildren(createResultTree(child)));
 
         return dependencyTreeResult;
+    }
+
+    private static void aggregateMIC(DependencyTreeNode dependencyTree, DependencyTreeResult dependencyTreeResult) {
+        dependencyTree.getReachableMethodsAtDistance().forEach((distance, reachability) -> {
+            Map<CtBehavior, Set<MethodCall>> reachableMethods = reachability.getReachableMethods();
+
+            // Calculate metric
+            Integer result = reachableMethods.values().stream().map(Set::size).reduce(0, Integer::sum);
+            dependencyTreeResult.addMicAtDistance(distance, result);
+
+            // Calculate distribution per class
+            reachableMethods.values().forEach(methodCalls -> {
+                methodCalls.forEach(methodCall -> dependencyTreeResult.addMicConnectionFromClass(methodCall.where().getDeclaringClass().getName()));
+            });
+        });
+    }
+
+    private static void aggregateAC(DependencyTreeNode dependencyTree, DependencyTreeResult dependencyTreeResult) {
+        dependencyTree.getReachableFieldsAtDistance().forEach((distance, reachability) -> {
+            Map<CtClass, Set<CtField>> reachableFields = reachability.getReachableFields();
+
+            // Calculate metric
+            Integer result = reachableFields.values().stream().map(Set::size).reduce(0, Integer::sum);
+            dependencyTreeResult.addAcAtDistance(distance, result);
+
+            // Calculate distribution per class
+            reachableFields.values().forEach(ctFields -> {
+                ctFields.forEach(ctField -> dependencyTreeResult.addAcConnectionFromClass(ctField.getDeclaringClass().getName()));
+            });
+        });
     }
 }
