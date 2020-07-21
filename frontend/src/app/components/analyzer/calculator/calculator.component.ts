52cba2dc8b42a1a0670e5286d325fc50489a1277
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-07-21 11:58:33 +0200

    Changed update of formula and onChange method of the calculator component

--- frontend/src/app/components/analyzer/calculator/calculator.component.ts
+++ frontend/src/app/components/analyzer/calculator/calculator.component.ts
@@ -10,28 +10,38 @@ export class CalculatorComponent implements OnInit {
   title = 'Formula to calculate degree of dependency';
   micFormula: string;
   acFormula: string;
-  acX = 1;
-  micX = 1;
+  tacX: number;
+  tmicX: number;
 
   constructor() {
   }
 
   ngOnInit(): void {
-    this.buildMICFormula();
-    this.buildACFormula();
+    this.tacX = 1;
+    this.tmicX = 1;
+    this.buildMicFormula('x');
+    this.buildAcFormula('x');
   }
 
-  private buildMICFormula() {
+  tacFactorChange() {
+    // Emit event
+  }
+
+  tmicFactorChange() {
+    // Emit event
+  }
+
+  private buildMicFormula(factor) {
     this.micFormula = `\\begin{equation*}
                          \\verb|TMIC| = \\sum_{\\verb|distance|}
-                         \\frac{\\verb|MIC|(\\verb|distance|)}{\\verb|distance| \\times ${this.micX}}
+                         \\frac{\\verb|MIC|(\\verb|distance|)}{\\verb|distance| \\times ${factor}}
                         \\end{equation*}`;
   }
 
-  private buildACFormula() {
+  private buildAcFormula(factor) {
     this.acFormula = `\\begin{equation*}
                         \\verb|TAC| = \\sum_{\\verb|distance|}
-                        \\frac{\\verb|AC|(\\verb|distance|)}{\\verb|distance| \\times ${this.acX}}
+                        \\frac{\\verb|AC|(\\verb|distance|)}{\\verb|distance| \\times ${factor}}
                       \\end{equation*}`;
   }
 
