692a1e9439e44b73c9e5b97c7eb5bd3320449c75
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-07-21 12:38:32 +0200

    Add micDistance and acDistance to DependencyTreeNode model

--- frontend/src/app/models/dependencyTree/node.ts
+++ frontend/src/app/models/dependencyTree/node.ts
@@ -1,11 +1,13 @@
 import {NodeColor} from './color';
+import {MetricDistance} from './metric.distance';
 
 export class TreeNodeFactory {
   static create({
                   id = 0, groupID = '', artifactID = '', version = '',
-                  title = '', level = 0, color = null, font = {multi: 'md'}
+                  title = '', level = 0, color = null, font = {multi: 'md'},
+                  micDistance = null, acDistance = null
                 }): TreeNode {
-    return new TreeNode(id, groupID, artifactID, version, title, level, color, font);
+    return new TreeNode(id, groupID, artifactID, version, title, level, color, font, micDistance, acDistance);
   }
 }
 
@@ -24,8 +26,16 @@ export class TreeNode implements ITreeNode {
   groupID: string;
   artifactID: string;
   version: string;
+  micDistance: MetricDistance;
+  acDistance: MetricDistance;
+  tac: number;
+  tmic: number;
 
-  constructor(id: number, groupID: string, artifactID: string, version: string, title: string, level: number, color: NodeColor, font: any) {
+  constructor(id: number, groupID: string,
+              artifactID: string, version: string,
+              title: string, level: number,
+              color: NodeColor, font: any,
+              micDistance: MetricDistance, acDistance: MetricDistance) {
     this.id = id;
     this.groupID = groupID;
     this.artifactID = artifactID;
@@ -35,9 +45,21 @@ export class TreeNode implements ITreeNode {
     this.color = color;
     this.font = font;
     this.label = `*Group Id:* ${groupID}\n*Artifact Id:* ${artifactID}\n*Version:* ${version}`;
+    this.micDistance = micDistance;
+    this.acDistance = acDistance;
+    this.tmic = 0;
+    this.tac = 0;
   }
 
   getLibraryCompleteName(): string {
     return `${this.groupID}.${this.artifactID}.${this.version}`;
+  }
+
+  calculateTmic(factor: number) {
+    this.tmic = this.micDistance.calculateMetric(factor);
+  }
+
+  calculateTac(factor: number) {
+    this.tac = this.acDistance.calculateMetric(factor);
   }
 }
