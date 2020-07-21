0717b59a61c5564d1e4afd90b1afcf5f0c5e05f0
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-07-21 12:31:25 +0200

    Add MetricDistance model

---
+++ frontend/src/app/models/dependencyTree/metric.distance.ts
@@ -0,0 +1,36 @@
+export class MetricDistanceFactory {
+  static create(mapJson: {}) {
+    const metricDistance = new MetricDistance();
+    for (const distance in mapJson) {
+      if (mapJson.hasOwnProperty(distance)) {
+        metricDistance.addDistance(Number(distance), mapJson[distance]);
+      }
+    }
+  }
+}
+
+export class MetricDistance {
+  private _distanceMap: Map<number, number>;
+
+  constructor() {
+    this._distanceMap = new Map<number, number>();
+  }
+
+  addDistance(distance: number, value: number) {
+    this._distanceMap.set(distance, value);
+  }
+
+  get distanceMap(): Map<number, number> {
+    return this._distanceMap;
+  }
+
+  calculateMetric(factor: number): number {
+    let result = 0;
+
+    for (const [distance, value] of this._distanceMap) {
+      result += value / (distance * factor);
+    }
+
+    return result;
+  }
+}
