61e747169c88827993e276a0c97403aa4518d169
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-07-22 11:21:04 +0200

    Add example bar chart in ClassDistributionComponent

--- frontend/src/app/components/analyzer/class-distribution/class-distribution.component.ts
+++ frontend/src/app/components/analyzer/class-distribution/class-distribution.component.ts
@@ -6,6 +6,13 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./class-distribution.component.css']
 })
 export class ClassDistributionComponent implements OnInit {
+  saleData = [
+    {name: 'Mobiles', value: 105000},
+    {name: 'Laptop', value: 55000},
+    {name: 'AC', value: 15000},
+    {name: 'Headset', value: 150000},
+    {name: 'Fridge', value: 20000}
+  ];
 
   constructor() {
   }
