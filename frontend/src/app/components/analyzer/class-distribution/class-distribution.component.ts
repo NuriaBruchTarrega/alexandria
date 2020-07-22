2ce3082a4d016b4d6af15ade2aa09396af1e1d39
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-07-22 11:48:07 +0200

    Add group vertical chart

--- frontend/src/app/components/analyzer/class-distribution/class-distribution.component.ts
+++ frontend/src/app/components/analyzer/class-distribution/class-distribution.component.ts
@@ -7,11 +7,110 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ClassDistributionComponent implements OnInit {
   saleData = [
-    {name: 'Mobiles', value: 105000},
-    {name: 'Laptop', value: 55000},
-    {name: 'AC', value: 15000},
-    {name: 'Headset', value: 150000},
-    {name: 'Fridge', value: 20000}
+    {
+      name: 'Germany',
+      series: [
+        {
+          name: '2010',
+          value: 40632,
+          extra: {
+            code: 'de'
+          }
+        },
+        {
+          name: '2000',
+          value: 36953,
+          extra: {
+            code: 'de'
+          }
+        },
+        {
+          name: '1990',
+          value: 31476,
+          extra: {
+            code: 'de'
+          }
+        }
+      ]
+    },
+    {
+      name: 'United States',
+      series: [
+        {
+          name: '2010',
+          value: 0,
+          extra: {
+            code: 'us'
+          }
+        },
+        {
+          name: '2000',
+          value: 45986,
+          extra: {
+            code: 'us'
+          }
+        },
+        {
+          name: '1990',
+          value: 37060,
+          extra: {
+            code: 'us'
+          }
+        }
+      ]
+    },
+    {
+      name: 'France',
+      series: [
+        {
+          name: '2010',
+          value: 36745,
+          extra: {
+            code: 'fr'
+          }
+        },
+        {
+          name: '2000',
+          value: 34774,
+          extra: {
+            code: 'fr'
+          }
+        },
+        {
+          name: '1990',
+          value: 29476,
+          extra: {
+            code: 'fr'
+          }
+        }
+      ]
+    },
+    {
+      name: 'United Kingdom',
+      series: [
+        {
+          name: '2010',
+          value: 36240,
+          extra: {
+            code: 'uk'
+          }
+        },
+        {
+          name: '2000',
+          value: 32543,
+          extra: {
+            code: 'uk'
+          }
+        },
+        {
+          name: '1990',
+          value: 26424,
+          extra: {
+            code: 'uk'
+          }
+        }
+      ]
+    }
   ];
 
   constructor() {
