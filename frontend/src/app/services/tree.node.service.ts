f5550cd44801930346f765cdfd8c7479863971a0
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-07-22 12:29:50 +0200

    Add generateChartData in TreeNodeService

--- frontend/src/app/services/tree.node.service.ts
+++ frontend/src/app/services/tree.node.service.ts
@@ -1,6 +1,9 @@
 import {Injectable} from '@angular/core';
 import {Metrics} from '../enumerations/metrics';
 import {DependencyTree} from '../models/dependencyTree/tree';
+import {TreeNode} from '../models/dependencyTree/node';
+import {ClassDistribution} from '../models/dependencyTree/class.distribution';
+import {ChartData, ChartDataFactory} from '../models/chart.data';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +19,21 @@ export class TreeNodeService {
     } else if (metric === Metrics.Tac) {
       dependencyTree.calculateTAC(factor);
     }
+  }
+
+  generateChartData(node: TreeNode): ChartData[] {
+    const micClassDistribution: ClassDistribution = node.micClassDistribution;
+    const acClassDistribution: ClassDistribution = node.acClassDistribution;
+
+    const chartDataArray: ChartData[] = [];
+    const classNames: Set<string> = new Set([...micClassDistribution.getAllClassNames(), ...acClassDistribution.getAllClassNames()]);
+
+    classNames.forEach(className => {
+      const micValue = micClassDistribution.getValue(className);
+      const acValue = acClassDistribution.getValue(className);
+      chartDataArray.push(ChartDataFactory.create(className, micValue, acValue));
+    });
+
+    return chartDataArray;
   }
 }
