b1a03e0053ed5c217258a119948a741b6fb910ed
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-08-02 15:32:18 +0200

    Add implementation of MetricCalculator

--- backend/src/main/java/nl/uva/alexandria/logic/metrics/calculators/MetricCalculator.java
+++ backend/src/main/java/nl/uva/alexandria/logic/metrics/calculators/MetricCalculator.java
@@ -1,4 +1,21 @@
 package nl.uva.alexandria.logic.metrics.calculators;
 
+import nl.uva.alexandria.logic.ClassPoolManager;
+import nl.uva.alexandria.logic.metrics.inheritance.InheritanceDetector;
+import nl.uva.alexandria.model.DependencyTreeNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public abstract class MetricCalculator {
+    private static final Logger LOG = LoggerFactory.getLogger(MetricCalculator.class);
+
+    private final ClassPoolManager classPoolManager;
+    private final InheritanceDetector inheritanceDetector;
+
+    public MetricCalculator(ClassPoolManager classPoolManager, InheritanceDetector inheritanceDetector) {
+        this.classPoolManager = classPoolManager;
+        this.inheritanceDetector = inheritanceDetector;
+    }
+
+    public abstract DependencyTreeNode calculateMetric(DependencyTreeNode dependencyTreeNode);
 }
