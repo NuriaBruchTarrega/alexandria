d165b2b0278b4a14b0e92642ed244fb8cfe4a48c
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-08-02 15:28:10 +0200

    Add implementation of InheritanceDetector

--- backend/src/main/java/nl/uva/alexandria/logic/metrics/inheritance/InheritanceDetector.java
+++ backend/src/main/java/nl/uva/alexandria/logic/metrics/inheritance/InheritanceDetector.java
@@ -1,4 +1,28 @@
 package nl.uva.alexandria.logic.metrics.inheritance;
 
+import javassist.CtClass;
+import javassist.NotFoundException;
+import nl.uva.alexandria.logic.ClassPoolManager;
+import nl.uva.alexandria.model.DependencyTreeNode;
+import nl.uva.alexandria.model.Library;
+
+import java.util.Set;
+
 public abstract class InheritanceDetector {
+    protected final ClassPoolManager classPoolManager;
+    protected Set<CtClass> currentLibraryClasses;
+    protected Library currentLibrary = null;
+
+    public InheritanceDetector(ClassPoolManager classPoolManager) {
+        this.classPoolManager = classPoolManager;
+    }
+
+    public abstract void calculateInheritanceOfDependencyTreeNode(DependencyTreeNode dependencyTreeNode) throws NotFoundException;
+
+    protected void updateCurrentLibrary(Library library) throws NotFoundException {
+        if (!library.equals(currentLibrary)) {
+            this.currentLibrary = library;
+            this.currentLibraryClasses = classPoolManager.getLibraryClasses(library.getLibraryPath());
+        }
+    }
 }
