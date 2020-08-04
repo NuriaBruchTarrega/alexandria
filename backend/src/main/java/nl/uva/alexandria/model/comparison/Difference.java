34418baece340c543223e6c478d653d1bf0a44fa
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-08-04 18:26:42 +0200

    Update Difference model

--- backend/src/main/java/nl/uva/alexandria/model/comparison/Difference.java
+++ backend/src/main/java/nl/uva/alexandria/model/comparison/Difference.java
@@ -5,31 +5,49 @@ import java.util.Objects;
 
 public class Difference {
     private String library;
-    private Integer numDetectedDifference;
-    private List<String> detectedOnlyByAnalysis;
-    private List<String> detectedOnlyByPaper;
+    private Integer numDirectDifference;
+    private Integer numTransitiveDifference;
+    private List<String> onlyByAnalysisDirect;
+    private List<String> onlyByPaperDirect;
+    private List<String> onlyByAnalysisTransitive;
+    private List<String> onlyByPaperTransitive;
 
-    public Difference(String library, Integer numDetectedDifference, List<String> detectedOnlyByAnalysis, List<String> detectedOnlyByPaper) {
+    public Difference(String library, Integer numDirectDifference, Integer numTransitiveDifference, List<String> onlyByAnalysisDirect, List<String> onlyByPaperDirect, List<String> onlyByAnalysisTransitive, List<String> onlyByPaperTransitive) {
         this.library = library;
-        this.numDetectedDifference = numDetectedDifference;
-        this.detectedOnlyByAnalysis = detectedOnlyByAnalysis;
-        this.detectedOnlyByPaper = detectedOnlyByPaper;
+        this.numDirectDifference = numDirectDifference;
+        this.numTransitiveDifference = numTransitiveDifference;
+        this.onlyByAnalysisDirect = onlyByAnalysisDirect;
+        this.onlyByPaperDirect = onlyByPaperDirect;
+        this.onlyByAnalysisTransitive = onlyByAnalysisTransitive;
+        this.onlyByPaperTransitive = onlyByPaperTransitive;
     }
 
     public String getLibrary() {
         return library;
     }
 
-    public Integer getNumDetectedDifference() {
-        return numDetectedDifference;
+    public Integer getNumDirectDifference() {
+        return numDirectDifference;
     }
 
-    public List<String> getDetectedOnlyByAnalysis() {
-        return detectedOnlyByAnalysis;
+    public Integer getNumTransitiveDifference() {
+        return numTransitiveDifference;
     }
 
-    public List<String> getDetectedOnlyByPaper() {
-        return detectedOnlyByPaper;
+    public List<String> getOnlyByAnalysisDirect() {
+        return onlyByAnalysisDirect;
+    }
+
+    public List<String> getOnlyByPaperDirect() {
+        return onlyByPaperDirect;
+    }
+
+    public List<String> getOnlyByAnalysisTransitive() {
+        return onlyByAnalysisTransitive;
+    }
+
+    public List<String> getOnlyByPaperTransitive() {
+        return onlyByPaperTransitive;
     }
 
     @Override
