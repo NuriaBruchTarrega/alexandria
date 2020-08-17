a261173bc57ec32c614f84b9bc20a5487492f990
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-08-17 11:26:20 +0200

    Add string constants in ClassNameUtils

--- backend/src/main/java/nl/uva/alexandria/logic/utils/ClassNameUtils.java
+++ backend/src/main/java/nl/uva/alexandria/logic/utils/ClassNameUtils.java
@@ -1,45 +1,48 @@
 package nl.uva.alexandria.logic.utils;
 
-import nl.uva.alexandria.model.Library;
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static nl.uva.alexandria.logic.LocalRepo.LOCAL_REPO_BASE_PATH;
-
 public class ClassNameUtils {
 
     private static final String JAR = ".jar";
+    private static final String CLASS = ".class";
+    private static final String EMPTY = "";
+    private static final String DOT = ".";
+    private static final String SEMICOLON = ";";
+    private static final String SLASH = "/";
+    private static final String ARRAY = "[";
+    private static final String SIGNATURE_REGEX = "[-<;>+*]+";
 
     private ClassNameUtils() {
     }
 
     public static String getFullyQualifiedNameFromClassPath(String classPath) {
-        classPath = classPath.replace(File.separator, "/");
-        return classPath.replace(".class", "").replace("/", ".");
+        classPath = classPath.replace(File.separator, SLASH);
+        return classPath.replace(CLASS, EMPTY).replace(SLASH, DOT);
     }
 
     public static String signatureToClassName(String signature) {
-        while (signature.startsWith("[")) signature = signature.substring(1); // Remove all the nested array levels
+        while (signature.startsWith(ARRAY)) signature = signature.substring(1); // Remove all the nested array levels
         signature = signature.substring(1); // Remove the L in case it is an object, and the primitive type in case it is
 
         if (signature.length() == 0) return signature;
 
         // Signatures end with ';', which has to be removed to obtain the name of the class.
         // If the signature comes from a generic signature, it may not end in ';'
-        if (signature.endsWith(";")) signature = signature.substring(0, signature.length() - 1);
+        if (signature.endsWith(SEMICOLON)) signature = signature.substring(0, signature.length() - 1);
 
-        return signature.replace("/", ".");
+        return signature.replace(SLASH, DOT);
     }
 
     public static List<String> getClassNamesFromGenericSignature(String signature) {
         List<String> classNames = new ArrayList<>();
 
-        List<String> split = Arrays.asList(signature.split("[-<;>+*]+"));
-        List<String> types = split.stream().filter(s -> s.contains("/")).collect(Collectors.toList()); // Discard non-specified types and primitives
+        List<String> split = Arrays.asList(signature.split(SIGNATURE_REGEX));
+        List<String> types = split.stream().filter(s -> s.contains(SLASH)).collect(Collectors.toList()); // Discard non-specified types and primitives
 
         types.forEach(t -> {
             String className = signatureToClassName(t); // Here, arrays and primitives are discarded
