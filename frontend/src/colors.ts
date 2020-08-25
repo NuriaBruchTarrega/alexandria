cbe8d48ea4c9060a6dd06bdb15e881ca9f84ca6b
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-08-25 13:16:20 +0200

    Add color constants file

---
+++ frontend/src/colors.ts
@@ -0,0 +1,7 @@
+export const PINK = '#e91e63';
+export const LIGHT_PINK = '#ff80ab';
+export const BLUE = '#3F51B5';
+export const LIGHT_BLUE = '#7986cb';
+export const LIGHTER_BLUE = '#9fa8da';
+export const GREY = '#cccccc';
+
