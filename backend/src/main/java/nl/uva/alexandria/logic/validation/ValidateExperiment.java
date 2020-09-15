e0cb5c3285fb674f3a6463498430ae70fd5fd642
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-09-15 17:03:30 +0200

    Add implementation of run in ValidateExperiment

--- backend/src/main/java/nl/uva/alexandria/logic/validation/ValidateExperiment.java
+++ backend/src/main/java/nl/uva/alexandria/logic/validation/ValidateExperiment.java
@@ -1,14 +1,62 @@
 package nl.uva.alexandria.logic.validation;
 
+import nl.uva.alexandria.logic.Analyzer;
+import nl.uva.alexandria.model.DependencyTreeResult;
 import nl.uva.alexandria.model.validation.AnalysisSummary;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Component
 public class ValidateExperiment {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ValidateExperiment.class);
+
     public static Set<AnalysisSummary> run(String pathToFile) {
-        return new HashSet<>();
+        File file = new File(pathToFile);
+        return runAnalysis(file);
+    }
+
+    private static Set<AnalysisSummary> runAnalysis(File file) {
+        Set<AnalysisSummary> analysisSummarySet = new HashSet<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String line = bufferedReader.readLine();
+
+            while (line != null) {
+                // Read the line
+                String[] values = line.split("\t");
+                if (values.length == 3) {
+                    Optional<AnalysisSummary> analysisSummaryOpt = analyzeLibrary(values[0], values[1], values[2]);
+                    analysisSummaryOpt.ifPresent(analysisSummarySet::add);
+                }
+                line = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return analysisSummarySet;
+    }
+
+    private static Optional<AnalysisSummary> analyzeLibrary(String groupId, String artifactId, String version) {
+        Analyzer analyzer = new Analyzer();
+
+        try {
+            DependencyTreeResult result = analyzer.analyze(groupId, artifactId, version).getDependencyTreeResult();
+            LOG.info("Finalized analysis of: {}:{}:{}", groupId, artifactId, version);
+            return Optional.of(AnalysisSummary.from(result));
+        } catch (RuntimeException e) {
+            LOG.info("Analysis of: {}:{}:{} did not work, because {}", groupId, artifactId, version, e.getMessage());
+        }
+
+        return Optional.empty();
     }
 }
