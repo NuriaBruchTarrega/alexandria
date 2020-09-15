8de0bc27143b7b427711605ed11b31bf2aabd617
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-09-15 17:03:15 +0200

    Add static factory method in AnalysisSummary (not implemented)

--- backend/src/main/java/nl/uva/alexandria/model/validation/AnalysisSummary.java
+++ backend/src/main/java/nl/uva/alexandria/model/validation/AnalysisSummary.java
@@ -1,4 +1,9 @@
 package nl.uva.alexandria.model.validation;
 
+import nl.uva.alexandria.model.DependencyTreeResult;
+
 public class AnalysisSummary {
+    public static AnalysisSummary from(DependencyTreeResult result) {
+        return new AnalysisSummary();
+    }
 }
