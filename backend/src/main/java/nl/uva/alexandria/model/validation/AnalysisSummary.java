7860667f08b71c2b020724fb453640d29f90022b
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-09-16 10:21:50 +0200

    Add implementation of static factory in AnalysisSummary

--- backend/src/main/java/nl/uva/alexandria/model/validation/AnalysisSummary.java
+++ backend/src/main/java/nl/uva/alexandria/model/validation/AnalysisSummary.java
@@ -1,9 +1,70 @@
 package nl.uva.alexandria.model.validation;
 
 import nl.uva.alexandria.model.DependencyTreeResult;
+import nl.uva.alexandria.model.Library;
+
+import java.util.*;
 
 public class AnalysisSummary {
+
+    private final String groupId;
+    private final String artifactId;
+    private final String version;
+    private Map<String, Boolean> isCouplingEnoughMap = new HashMap<>();
+    private Set<String> couplingNotEnoughLibraries = new HashSet<>();
+
+    private AnalysisSummary(String groupId, String artifactId, String version) {
+        this.groupId = groupId;
+        this.artifactId = artifactId;
+        this.version = version;
+    }
+
     public static AnalysisSummary from(DependencyTreeResult result) {
-        return new AnalysisSummary();
+        Library clientLibrary = result.getLibrary();
+        AnalysisSummary newAnalysisSummary = new AnalysisSummary(clientLibrary.getGroupID(), clientLibrary.getArtifactID(), clientLibrary.getVersion());
+
+        Queue<DependencyTreeResult> toVisit = new ArrayDeque<>(result.getChildren());
+
+        while (!toVisit.isEmpty()) {
+            DependencyTreeResult visiting = toVisit.poll();
+            boolean isCouplingEnough =
+                    !visiting.getAcAtDistance().isEmpty() || !visiting.getMicAtDistance().isEmpty()
+                            || (visiting.getNumReachableClasses() == 0 && visiting.getNumReachableBehaviors() == 0);
+            newAnalysisSummary.putToIsCouplingEnoughMap(visiting.getLibrary().toString(), isCouplingEnough);
+
+            if (!isCouplingEnough) newAnalysisSummary.addToCouplingNotEnoughLibraries(visiting.getLibrary().toString());
+
+            toVisit.addAll(visiting.getChildren());
+        }
+
+        return newAnalysisSummary;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public String getArtifactId() {
+        return artifactId;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public Map<String, Boolean> getIsCouplingEnoughMap() {
+        return isCouplingEnoughMap;
+    }
+
+    public Set<String> getCouplingNotEnoughLibraries() {
+        return couplingNotEnoughLibraries;
+    }
+
+    private void putToIsCouplingEnoughMap(String library, boolean isCouplingEnough) {
+        this.isCouplingEnoughMap.put(library, isCouplingEnough);
+    }
+
+    private void addToCouplingNotEnoughLibraries(String library) {
+        this.couplingNotEnoughLibraries.add(library);
     }
 }
