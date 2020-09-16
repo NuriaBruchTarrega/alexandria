8b2ac98c34f297a35c19642abd9ec1f832b9563c
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-09-16 10:32:20 +0200

    Add equals and hashCode in AnalysisSummary

--- backend/src/main/java/nl/uva/alexandria/model/validation/AnalysisSummary.java
+++ backend/src/main/java/nl/uva/alexandria/model/validation/AnalysisSummary.java
@@ -67,4 +67,19 @@ public class AnalysisSummary {
     private void addToCouplingNotEnoughLibraries(String library) {
         this.couplingNotEnoughLibraries.add(library);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AnalysisSummary that = (AnalysisSummary) o;
+        return groupId.equals(that.groupId) &&
+                artifactId.equals(that.artifactId) &&
+                version.equals(that.version);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(groupId, artifactId, version);
+    }
 }
