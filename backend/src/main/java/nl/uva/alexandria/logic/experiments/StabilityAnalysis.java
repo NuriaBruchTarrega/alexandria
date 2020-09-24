9a645b06b1c0ed0715d1d30961f4e15d1b495ef3
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-09-24 14:55:16 +0200

    Add method getStabilityAnalysisData in StabilityAnalysis

--- backend/src/main/java/nl/uva/alexandria/logic/experiments/StabilityAnalysis.java
+++ backend/src/main/java/nl/uva/alexandria/logic/experiments/StabilityAnalysis.java
@@ -1,13 +1,16 @@
 package nl.uva.alexandria.logic.experiments;
 
 import nl.uva.alexandria.model.DependencyTreeResult;
+import nl.uva.alexandria.model.experiments.StabilityAnalysisData;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 @Component
 public class StabilityAnalysis {
@@ -16,7 +19,13 @@ public class StabilityAnalysis {
     }
 
     public static void run(String pathToFile) {
+        Set<StabilityAnalysisData> stabilityAnalysisDataSet = getStabilityAnalysisData(pathToFile);
+        stabilityAnalysisDataSet.forEach(stabilityAnalysisData -> stabilityAnalysisData.runStabilityAnalysis());
+    }
+
+    private static Set<StabilityAnalysisData> getStabilityAnalysisData(String pathToFile) {
         File file = new File(pathToFile);
+        Set<StabilityAnalysisData> stabilityAnalysisDataSet = new HashSet<>();
 
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             String line = bufferedReader.readLine();
@@ -25,14 +34,14 @@ public class StabilityAnalysis {
                 String[] values = "\t".split(line);
                 if (values.length == 3) {
                     Optional<DependencyTreeResult> dependencyTreeResultOptional = AnalysisRunner.analyzeLibrary(values[0], values[1], values[2]);
-                    dependencyTreeResultOptional.ifPresent(dependencyTreeResult -> {
-                        // TODO: stability analysis
-                    });
+                    dependencyTreeResultOptional.ifPresent(dependencyTreeResult -> stabilityAnalysisDataSet.addAll(StabilityAnalysisData.from(dependencyTreeResult)));
                 }
                 line = bufferedReader.readLine();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return stabilityAnalysisDataSet;
     }
 }
