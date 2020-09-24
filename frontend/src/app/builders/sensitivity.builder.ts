a0732132a29ea6e6f71d39a7f2465d92633085d8
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-09-24 18:47:29 +0200

    Implemented Sensitivity builder

--- frontend/src/app/builders/sensitivity.builder.ts
+++ frontend/src/app/builders/sensitivity.builder.ts
@@ -1,4 +1,12 @@
-export function buildSensitivityAnalysis(res) {
-  // TODO: implementation
-  console.log(res);
+import {SensitivityResult, SensitivityResultFactory} from '@models/experiments/sensitivity-result';
+
+export function buildSensitivityAnalysis(res): Set<SensitivityResult> {
+  const sensitivityResultSet: Set<SensitivityResult> = new Set<SensitivityResult>();
+
+  res.forEach(sensitivityAnalysisData => {
+    const sensitivityResult: SensitivityResult = SensitivityResultFactory.create(sensitivityAnalysisData);
+    sensitivityResultSet.add(sensitivityResult);
+  });
+
+  return sensitivityResultSet;
 }
