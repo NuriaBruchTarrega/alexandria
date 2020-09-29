0e06ad84d3db9712f5d5e4241b1349f874d368b3
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-09-29 15:40:26 +0200

    Delete add "_dependencies" in excelService

--- frontend/src/app/services/excel.service.ts
+++ frontend/src/app/services/excel.service.ts
@@ -22,6 +22,6 @@ export class ExcelService {
 
   saveExcelFile(buffer: any, fileName: string) {
     const data: Blob = new Blob([buffer], {type: EXCEL_TYPE});
-    FileSaver.saveAs(data, fileName + '_dependencies' + EXCEL_EXTENSION);
+    FileSaver.saveAs(data, fileName + EXCEL_EXTENSION);
   }
 }
