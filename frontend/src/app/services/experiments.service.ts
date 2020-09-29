2ee20a22d720a9f2e2a4ed68446c76794747a54b
NuriaBruchTarrega <nuria.bruch@gmail.com>
2020-09-29 17:30:20 +0200

    Add experiments service request for benchmarking

--- frontend/src/app/services/experiments.service.ts
+++ frontend/src/app/services/experiments.service.ts
@@ -3,6 +3,8 @@ import {environment} from '@src/environments/environment';
 import {HttpClient} from '@angular/common/http';
 import {map} from 'rxjs/operators';
 import {buildSensitivityAnalysis} from '@builders/sensitivity.builder';
+import {Observable} from 'rxjs';
+import {SensitivityResult} from '@models/experiments/sensitivity-result';
 
 const apiBaseUrl = environment.apiBaseUrl;
 
@@ -14,9 +16,14 @@ export class ExperimentsService {
   constructor(private httpClient: HttpClient) {
   }
 
-  sensitivityAnalysis(pathToFile: string) {
+  sensitivityAnalysis(pathToFile: string): Observable<Set<SensitivityResult>> {
     const result = this.httpClient.post(`${apiBaseUrl}/sensitivity`, {pathToFile})
       .pipe(map(res => buildSensitivityAnalysis(res)));
     return result;
+  }
+
+  benchmarkRequest(path: string): Observable<any> {
+    // TODO: to request
+    return new Observable<any>();
   }
 }
